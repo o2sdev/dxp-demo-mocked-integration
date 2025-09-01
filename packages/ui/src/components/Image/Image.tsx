ee9b3cb99649a72fc72053d04a32cafb1babdc7f
Marcin Krasowski <marcin.krasowski@hycom.pl>
2025-09-01 11:27:27 +0200

    feat: enabled next.js image optimizations

--- packages/ui/src/components/Image/Image.tsx
+++ packages/ui/src/components/Image/Image.tsx
@@ -5,14 +5,22 @@ import React from 'react';
 
 import { ImageProps } from './Image.types';
 
-const imageLoader: ImageLoader = ({ src, width, quality }) => {
+const _imageLoader: ImageLoader = ({ src, width, quality }) => {
     return `${src}?w=${width}&q=${quality || 99}&fm=webp`;
 };
 
 export const Image: React.FC<ImageProps> = ({ src, alt, width, height, fill, ...rest }) => {
     if ((width && height) || fill) {
         return (
-            <NextImage src={src} alt={alt} width={width} height={height} fill={fill} loader={imageLoader} {...rest} />
+            <NextImage
+                src={src}
+                alt={alt}
+                width={width}
+                height={height}
+                fill={fill}
+                //loader={imageLoader}
+                {...rest}
+            />
         );
     }
 
